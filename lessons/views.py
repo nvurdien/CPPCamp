29fd76b4d9d5c757a62fe8f8f36d0604042f1251
nvurdien <nvurdien@gmail.com>
2017-04-23 13:18:30 -0700

    chris is a god

--- lessons/views.py
+++ lessons/views.py
@@ -44,7 +44,7 @@ class DetailView(generic.DetailView):
         return Lesson.objects.order_by('lesson_id')
 
 
-class QuestionDetail(LoginRequiredMixin,generic.DetailView):
+class QuestionDetail(generic.DetailView):
     model = Lesson
     template_name = 'lessons/exercise/exercise.html'
     def get_queryset(self):
@@ -56,7 +56,7 @@ class QuestionDetail(LoginRequiredMixin,generic.DetailView):
     #    context['Question_list'] = Question.objects.filter('lesson_num = lesson_id')
     #    return context
 
-class QuizDetail(LoginRequiredMixin,generic.DetailView):
+class QuizDetail(generic.DetailView):
     model = Lesson
     template_name = 'lessons/quiz/quiz.html'
     def get_queryset(self):
@@ -81,12 +81,12 @@ def detail(request, lesson_id):
     lesson = get_object_or_404(Lesson, pk=lesson_id)
     return render(request, 'lessons/detail.html', {'lessons': lesson})
 
-@login_required
+
 def details(request, lesson_id):
     lesson = get_object_or_404(Lesson, pk=lesson_id)
     return render(request, 'lessons/exercise/exercise.html', {'lessons': lesson})
 
-@login_required
+
 def detailss(request, lesson_id):
     lesson = get_object_or_404(Lesson, pk=lesson_id)
     return render(request, 'lessons/quiz/quiz.html', {'lessons': lesson})
