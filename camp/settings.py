71f57d49d014a941ad085c56c9eeeb31bf041620
nvurdien <nvurdien@gmail.com>
2017-04-23 17:40:13 -0700

    Heroku

--- camp/settings.py
+++ camp/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATICFILES_STORAGE= 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = "/var/www/example.com/static/"
 STATICFILES_DIRS = [
